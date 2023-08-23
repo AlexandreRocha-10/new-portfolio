647a0ca39e29b83807b5b6392981ed2da71876bc
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-08-23 19:00:12 -0300

    dashboard proj

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import ProjectItem from "../components/ProjectItem";
 import iarabicalho from "../assets/iarabicalho.jpeg"
-import solarsystem from "../assets/solarsystem.jpeg";
+import trybewallet from "../assets/trybewallet.jpeg";
 import trybetunes from "../assets/trybetunes.jpeg";
 import tryunfo from "../assets/tryunfo.jpeg";
 
@@ -14,7 +14,7 @@ const Projects: FC = () => {
         <p className="text-center py-8">Here are some of the projects I've developed. Feel free to explore more on my GitHub profile. Any questions, suggestions, or critiques are welcome!</p>
         <div className="grid sm:grid-cols-2 gap-12">
             <ProjectItem img={iarabicalho} title='Iara Bicalho Website' tech='Typescript + React + Vite + Tailwind' githubLink='https://github.com/AlexandreRocha-10/iarabicalho-site' appLink='https://iarabicalho.vercel.app/' />
-            <ProjectItem img={solarsystem} title='Solar System' tech='React.JS' githubLink='#' appLink='#' />
+            <ProjectItem img={trybewallet} title='Trybe Wallet' tech='Typescript + React + Vite + Tailwind + Tremor' githubLink='https://github.com/AlexandreRocha-10/dashboard' appLink='https://trybe-wallet-rose-mu.vercel.app/' />
             <ProjectItem img={trybetunes} title='Trybetunes' tech='React.JS' githubLink='#' appLink='#' />
             <ProjectItem img={tryunfo} title='Tryunfo' tech='React.JS' githubLink='#' appLink='#' />
         </div>        
