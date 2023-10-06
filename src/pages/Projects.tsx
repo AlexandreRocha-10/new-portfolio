77c3a221663d5c895bd4edaac96cd260f65e2d9f
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-10-06 19:25:15 -0300

    mygpt

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import ProjectItem from "../components/ProjectItem";
 import iarabicalho from "../assets/iarabicalho.jpeg"
 import trybewallet from "../assets/trybewallet.jpeg";
-import trybetunes from "../assets/trybetunes.jpeg";
+import mygpt from "../assets/mygpt.jpeg";
 import uaisearch from "../assets/uaisearch.jpeg";
 
 
@@ -15,7 +15,7 @@ const Projects: FC = () => {
         <div className="grid sm:grid-cols-2 gap-12">
             <ProjectItem img={iarabicalho} title='Iara Bicalho Website' tech='Typescript + React + Vite + Tailwind' githubLink='https://github.com/AlexandreRocha-10/iarabicalho-site' appLink='https://iarabicalho.vercel.app/' />
             <ProjectItem img={trybewallet} title='Trybe Wallet' tech='Typescript + React + Vite + Tailwind + Tremor' githubLink='https://github.com/AlexandreRocha-10/dashboard' appLink='https://trybe-wallet-rose-mu.vercel.app/' />
-            <ProjectItem img={trybetunes} title='Trybetunes' tech='React.JS' githubLink='#' appLink='#' />
+            <ProjectItem img={mygpt} title='MyChatGPT Clone' tech='Next.js + Tailwind + OpenAI APIs' githubLink='https://github.com/AlexandreRocha-10/mygpt' appLink='https://mygptchat.vercel.app/' />
             <ProjectItem img={uaisearch} title='UaiSearch' tech='Next.js + Tailwind + Google APIs  ' githubLink='https://github.com/AlexandreRocha-10/discover' appLink='https://uaisearch.vercel.app/' />
         </div>        
     </div>
