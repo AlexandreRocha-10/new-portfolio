6ba2665b8e33e1c6b00766dff3d6c003cb50760d
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-10-09 14:26:17 -0300

    att proj

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -13,10 +13,10 @@ const Projects: FC = () => {
         <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
         <p className="text-center py-8">Here are some of the projects I've developed. Feel free to explore more on my GitHub profile. Any questions, suggestions, or critiques are welcome!</p>
         <div className="grid sm:grid-cols-2 gap-12">
+            <ProjectItem img={mygpt} title='MyChatGPT Clone' tech='Next.js + Tailwind + OpenAI APIs' githubLink='https://github.com/AlexandreRocha-10/mygpt' appLink='https://mygptchat.vercel.app/' />
+            <ProjectItem img={uaisearch} title='UaiSearch' tech='Next.js + Tailwind + Google APIs  ' githubLink='https://github.com/AlexandreRocha-10/uaisearch' appLink='https://uaisearch.vercel.app/' />
             <ProjectItem img={iarabicalho} title='Iara Bicalho Website' tech='Typescript + React + Vite + Tailwind' githubLink='https://github.com/AlexandreRocha-10/iarabicalho-site' appLink='https://iarabicalho.vercel.app/' />
             <ProjectItem img={trybewallet} title='Trybe Wallet' tech='Typescript + React + Vite + Tailwind + Tremor' githubLink='https://github.com/AlexandreRocha-10/dashboard' appLink='https://trybe-wallet-rose-mu.vercel.app/' />
-            <ProjectItem img={mygpt} title='MyChatGPT Clone' tech='Next.js + Tailwind + OpenAI APIs' githubLink='https://github.com/AlexandreRocha-10/mygpt' appLink='https://mygptchat.vercel.app/' />
-            <ProjectItem img={uaisearch} title='UaiSearch' tech='Next.js + Tailwind + Google APIs  ' githubLink='https://github.com/AlexandreRocha-10/discover' appLink='https://uaisearch.vercel.app/' />
         </div>        
     </div>
   )
